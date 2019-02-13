e49a2a74eba481c19b7dfa8675af34e057661dfc
Mojjammil <mojjammil@gmail.com>
2019-02-13 02:54:11 +0200

    Updated comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ num1 = float(input("Enter first number: "))
 op = input("Enter calculation operator: ")
 num2 = float(input("Enter second number: "))
 
-# Calculation function
+# Calculation functions
 if op == "+":
     print(num1 + num2)
 elif op == "-":
